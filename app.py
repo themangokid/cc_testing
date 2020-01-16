dce5efc9fd4ad8ad1df296b1b558a0ecb64ea85b
CarlanMan <59764199+CarlanMan@users.noreply.github.com>
2020-01-16 15:10:07 +0100

    kitten.jpg added to flask

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 
 # https://hackersandslackers.com/flask-sqlalchemy-database-models/
-from flask import Flask, escape, url_for, render_template
+from flask import Flask, escape, url_for, render_template, send_file
 app = Flask(__name__)
 
+#lade till flask, send_file
 
-@app.route("/")
-def main():
-    return '''Flask installed.  '''
+#@app.route("/")
+#def main():
+#    return '''Flask installed.  '''
+
+
+
+@app.route('/')
+def get_image():
+    filename = 'kitten.jpg'
+    return send_file(filename, mimetype='kitten.jpg')
+
 
 
 @app.route('/<name>')
